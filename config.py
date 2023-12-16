394fb5f151eea31f3af94b6732c4c5b34f4cf7cf
blutbad33 <151537369+blutbad33@users.noreply.github.com>
2023-12-16 08:35:46 +0300

    Update config.py
    The `config.py` script is a central configuration file for the cryptocurrency trading project, defining key parameters and settings:

--- config.py
+++ config.py
@@ -1,18 +1,23 @@
 # config.py
 
 # Binance API configuration
-API_KEY = 'your_binance_api_key'
-API_SECRET = 'your_binance_api_secret'
+API_KEY = 'rbQFZbq8uQIfVDUVciOyxGjjSTRYzzMt5ca9Xsarys5i9fZMaQZvgqWy17XDpRGU'
+API_SECRET = 'fV5xqoD2BzZ8KbO95Ug6vOC59UoHAabSquDBEv6Y4B7x4I32Q32Me7mdalrDog4V'
 
 # Trading configuration
 TRADING_PAIRS = ['BTCUSDT', 'ETHUSDT', 'XRPUSDT']  # Cryptocurrency pairs to trade
 TRADE_INTERVAL = '1h'  # Trading interval
 
-# Technical analysis settings
+# Technical analysis settings for RSI and Moving Average Strategy
 RSI_PERIOD = 14  # Period for Relative Strength Index
-MA_PERIOD = 50  # Period for Moving Average
-BB_PERIOD = 20  # Period for Bollinger Bands
-BB_STD_DEV = 2  # Standard deviation for Bollinger Bands
+OVERBOUGHT_RSI = 70
+OVERSOLD_RSI = 30
+SHORT_TERM_MA_PERIOD = 9  # Period for short-term EMA
+LONG_TERM_MA_PERIOD = 21  # Period for long-term EMA
+
+# Technical analysis settings for Bollinger Bands and RSI Strategy
+BOLLINGER_BANDS_PERIOD = 20  # Period for Bollinger Bands
+BOLLINGER_BANDS_STD_DEV = 2  # Standard deviation for Bollinger Bands
 
 # Risk management
 MAX_TRADE_QUANTITY = 10  # Maximum quantity to trade
