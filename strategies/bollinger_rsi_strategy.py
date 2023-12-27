13b51f362776e8f68a7df07c8305f2daba15a0fb
blutbad33 <151537369+blutbad33@users.noreply.github.com>
2023-12-27 12:36:48 +0300

    Update bollinger_rsi_strategy.py

--- strategies/bollinger_rsi_strategy.py
+++ strategies/bollinger_rsi_strategy.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import config
-from datetime import timedelta
 
 def bollinger_rsi_strategy(data):
     # Calculate Bollinger Bands using parameters from config.py
@@ -19,38 +18,8 @@ def bollinger_rsi_strategy(data):
     rs = gain / loss
     data['RSI'] = 100 - (100 / (1 + rs))
 
-    # Generate signals with additional conditions
-    for i in range(len(data)):
-        data.loc[i, 'Buy'] = False
-        data.loc[i, 'Sell'] = False
-        if i > 0 and not pd.isnull(data.loc[i, 'RSI']):
-            # Buy condition
-            if data.loc[i, 'close'] < data.loc[i, 'lower_band'] and data.loc[i, 'RSI'] < 30:
-                data.loc[i, 'Buy'] = True
-                # Check for sell condition within 1 day
-                sell_date = data.loc[i, 'timestamp'] + timedelta(days=1)
-                sell_index = data[(data['timestamp'] >= sell_date)].index.min()
-                if sell_index and sell_index < len(data):
-                    data.loc[sell_index, 'Sell'] = True
-            # Sell condition
-            elif data.loc[i, 'close'] > data.loc[i, 'upper_band'] and data.loc[i, 'RSI'] > 70:
-                data.loc[i, 'Sell'] = True
-
-    # Trade size calculation (an example approach)
-    data['Trade Size'] = data.apply(lambda row: calculate_trade_size(row['timestamp'], row['close']), axis=1)
+    # Generate signals
+    data['Buy'] = (data['close'] < data['lower_band']) & (data['RSI'] < config.OVERSOLD_RSI)
+    data['Sell'] = (data['close'] > data['upper_band']) & (data['RSI'] > config.OVERBOUGHT_RSI)
 
     return data
-
-def calculate_trade_size(timestamp, price):
-    # Example trade size calculation based on volatility or other factors
-    # Adjust this function according to your strategy
-    if timestamp.hour < 12:
-        return config.INITIAL_CAPITAL * 0.01  # 1% of capital for morning trades
-    else:
-        return config.INITIAL_CAPITAL * 0.02  # 2% of capital for afternoon trades
-
-# Example usage
-if __name__ == "__main__":
-    # Load data and apply strategy (assuming data is loaded correctly)
-    data = pd.DataFrame()  # Replace with actual data loading logic
-    strategy_data = bollinger_rsi_strategy(data)
