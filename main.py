72ad07fd9836f8ac9132694cb9e7627183c0cf5d
blutbad33 <151537369+blutbad33@users.noreply.github.com>
2023-12-27 14:55:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,26 +29,23 @@ def simulate_trades(data, strategy_name, pair, initial_capital, trade_results):
         exit_price = None
         profit_loss = 0
 
+        # Buy condition
         if data['Buy'].iloc[i] and capital > 0 and not entry_price:
             entry_price = close_price
             stop_loss_price = entry_price - (initial_capital * config.STOP_LOSS_PERCENTAGE)
             trade_size, risk_per_trade = calculate_trade_size(cumulative_pnl, entry_price, stop_loss_price)
             entry_time = timestamp
             capital -= trade_size * entry_price
-        elif entry_time and (timestamp - entry_time >= timedelta(hours=24)):
+
+        # Sell condition or auto-sell after 24 hours
+        if (data['Sell'].iloc[i] or (entry_time and timestamp - entry_time >= timedelta(hours=24))) and entry_price is not None:
             exit_price = close_price
-            profit_loss = trade_size * (exit_price - entry_price)
+            profit_loss = trade_size * (exit_price - entry_price) if trade_size is not None else 0
             cumulative_pnl += profit_loss
-            capital += trade_size * exit_price
+            capital += trade_size * exit_price if trade_size is not None else 0
             entry_price = None
 
-        if data['Sell'].iloc[i] and entry_price is not None:
-            exit_price = close_price
-            profit_loss = trade_size * (exit_price - entry_price)
-            cumulative_pnl += profit_loss
-            capital += trade_size * exit_price
-            entry_price = None
-
+        # Append trade result
         if exit_price is not None:
             trade_duration = (timestamp - entry_time).total_seconds() / 3600 if entry_time else 0
             trade_results.append({
@@ -56,16 +53,17 @@ def simulate_trades(data, strategy_name, pair, initial_capital, trade_results):
                 'Trade Duration (hrs)': trade_duration,
                 'Strategy Identifier': strategy_name,
                 'Trading Pair': pair,
-                'Trade Size': trade_size,
-                'Risk Per Trade': risk_per_trade,
-                'Entry Price': entry_price,
-                'Exit Price': exit_price,
+                'Trade Size': trade_size if trade_size is not None else 0,
+                'Risk Per Trade': risk_per_trade if risk_per_trade is not None else 0,
+                'Entry Price': entry_price if entry_price is not None else 0,
+                'Exit Price': exit_price if exit_price is not None else 0,
                 'Profit/Loss': profit_loss,
                 'Cumulative Profit/Loss': cumulative_pnl,
                 'Position Status': 'Closed'
             })
-            trade_size = None
-            risk_per_trade = None
+            entry_price = None  # Reset for the next trade
+            trade_size = None  # Reset for the next trade
+            risk_per_trade = None  # Reset for the next trade
 
     return capital
 
